dd4da531fa08a1e20c1fd8b9ebed28779c96e3f2
Kosy Anyanwu <kosyfrances@gmail.com>
2017-12-30 10:26:48 +0100

    Change mode

--- dev/rundeck.sh
+++ dev/rundeck.sh
@@ -7,6 +7,7 @@
 ### Note: You can access Rundeck at http://localhost:4440/user/login
 ### Username:admin, password: admin
 
+set -eo pipefail
 
 docker run -p 4440:4440 \
     -e EXTERNAL_SERVER_URL=http://localhost:4440 \
