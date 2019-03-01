afabbd1dca993740de473b84dc6ecbfcbb6a85d0
Kosy Anyanwu <kosyfrances@gmail.com>
2019-03-01 11:52:20 +0100

    cli setup: Code cleanup/Refactor
    Add api_key to config file during setup
    Use logrus instead of Prometheus log
    Remove redundant comments

--- cli/cmd/setup.go
+++ cli/cmd/setup.go
@@ -1,27 +1,14 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
 	"fmt"
 	"strings"
 
-	"github.com/kosyfrances/rundeck-zabbix/lib"
-	"github.com/prometheus/common/log"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+
+	"github.com/kosyfrances/rundeck-zabbix/lib"
+	"github.com/kosyfrances/rundeck-zabbix/lib/zabbix"
 )
 
 // setupCmd represents the setup command
@@ -29,63 +16,7 @@ var setupCmd = &cobra.Command{
 	Use:   "setup",
 	Short: "initialise tool configuration",
 	Long:  `initialise tool configuration`,
-	Run: func(cmd *cobra.Command, args []string) {
-		var zabbixURL, zabbixUser, zabbixPassword, rundeckURL, rundeckAPIKey string
-		zabbixConfigKeyName, rundeckConfigKeyName := lib.CONFIG_ZABBIX_KEY_NAME, lib.CONFIG_RUNDECK_KEY_NAME
-		if viper.IsSet(zabbixConfigKeyName) {
-			zabbixURL = getConfigValueByKey(zabbixConfigKeyName + "." + lib.CONFIG_URL_KEY_NAME)
-			zabbixUser = getConfigValueByKey(zabbixConfigKeyName + "." + lib.CONFIG_USERNAME_KEY_NAME)
-			zabbixPassword = getConfigValueByKey(zabbixConfigKeyName + "." + lib.CONFIG_PASSWORD_KEY_NAME)
-		}
-
-		fmt.Println(appendValueToPrompt("Enter the Zabbix Server URL:", zabbixURL))
-		fmt.Scanln(&zabbixURL)
-
-		fmt.Println(appendValueToPrompt("Enter the Zabbix Username:", zabbixUser))
-		fmt.Scanln(&zabbixUser)
-
-		fmt.Println(appendValueToPrompt("Enter the Zabbix Password:", zabbixPassword))
-		fmt.Scanln(&zabbixPassword)
-
-		if viper.IsSet(rundeckConfigKeyName) {
-			rundeckURL = getConfigValueByKey(rundeckConfigKeyName + "." + lib.CONFIG_URL_KEY_NAME)
-			rundeckAPIKey = getConfigValueByKey(rundeckConfigKeyName + "." + lib.CONFIG_API_KEY_KEY_NAME)
-		}
-		fmt.Println(appendValueToPrompt("Enter the Rundeck URL:", rundeckURL))
-		fmt.Scanln(&rundeckURL)
-
-		fmt.Println(appendValueToPrompt("Enter the Rundeck API_KEY:", rundeckAPIKey))
-		fmt.Scanln(&rundeckAPIKey)
-
-		zabbixConfig := lib.ZabbixConfig{
-			Url:      zabbixURL,
-			UserName: zabbixUser,
-			Password: zabbixPassword,
-		}
-
-		rundeckConfig := lib.RundeckConfig{
-			Url:    rundeckURL,
-			ApiKey: rundeckAPIKey,
-		}
-
-		newConfig := lib.Config{
-			Zabbix:  zabbixConfig,
-			Rundeck: rundeckConfig,
-		}
-
-		newConfig.Save()
-		log.Info("Configuration file generated in " + lib.ConfigDirectory)
-	},
-}
-
-// gets value from config file loaded via viper
-func getConfigValueByKey(key string) string {
-	var result string
-	value := viper.Get(key)
-	if value != nil {
-		result = value.(string)
-	}
-	return result
+	Run:   runSetup,
 }
 
 // mask values retrieved from config
@@ -106,14 +37,51 @@ func appendValueToPrompt(promptString, value string) string {
 
 func init() {
 	rootCmd.AddCommand(setupCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func runSetup(cmd *cobra.Command, args []string) {
+	cfg, err := lib.NewConfigFromFile(lib.ConfigPath)
+	if err != nil {
+		log.Infof("no config file yet. %v", err)
+		cfg = &lib.Config{}
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// setupCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// Read Zabbix config
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// setupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	fmt.Println(appendValueToPrompt("Enter the Zabbix Server URL:", cfg.Zabbix.URL))
+	fmt.Scanln(&cfg.Zabbix.URL)
+
+	fmt.Println(appendValueToPrompt("Enter the Zabbix Username:", cfg.Zabbix.UserName))
+	fmt.Scanln(&cfg.Zabbix.UserName)
+
+	fmt.Println(appendValueToPrompt("Enter the Zabbix Password:", cfg.Zabbix.Password))
+	fmt.Scanln(&cfg.Zabbix.Password)
+
+	// Read Rundeck config
+
+	fmt.Println(appendValueToPrompt("Enter the Rundeck URL:", cfg.Rundeck.URL))
+	fmt.Scanln(&cfg.Rundeck.URL)
+
+	fmt.Println(appendValueToPrompt("Enter the Rundeck API_KEY:", cfg.Rundeck.APIKey))
+	fmt.Scanln(&cfg.Rundeck.APIKey)
+
+	// Generate Zabbix API key
+	z, err := zabbix.CreateClientUsingAuth(cfg.Zabbix.URL, cfg.Zabbix.UserName, cfg.Zabbix.Password)
+	if err != nil {
+		log.Errorf("cannot create Zabbix API client. %v", err)
+		return
+	}
+
+	cfg.Zabbix.APIKey, err = z.GetKey()
+	if err != nil {
+		log.Errorf("cannot generate Zabbix API key. %v", err)
+		return
+	}
+
+	err = cfg.Save()
+	if err != nil {
+		log.Errorf("cannot save configuration. %v", err)
+	} else {
+		log.Infof("Configuration file generated in %v", lib.ConfigDirectory)
+	}
 }
