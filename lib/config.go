1bf9d711d36495c90003776fb854128dbf00f61c
Kosy Anyanwu <kosyfrances@gmail.com>
2019-03-01 11:50:00 +0100

    lib: Code cleanup
    Remove redundant comment/Add relevant comment for Golint
    Fix variables
    Add helper functions

--- lib/config.go
+++ lib/config.go
@@ -2,65 +2,84 @@ package lib
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 )
 
+// ZabbixConfig struct holds Zabbix configuration info
 type ZabbixConfig struct {
-	Url      string `json:"url"`
+	URL      string `json:"url"`
 	UserName string `json:"username"`
 	Password string `json:"password"`
+	APIKey   string `json:"api_key"`
 }
 
+// RundeckConfig struct holds Zabbix configuration info
 type RundeckConfig struct {
-	Url    string `json:"url"`
-	ApiKey string `json:"api_key"`
+	URL    string `json:"url"`
+	APIKey string `json:"api_key"`
 }
 
+// Config struct holds the setup config file info
 type Config struct {
 	Zabbix  ZabbixConfig  `json:"zabbix"`
 	Rundeck RundeckConfig `json:"rundeck"`
 }
 
 var (
-	UserHome        string
-	ConfigPath      string
+	// ConfigPath holds path to config
+	ConfigPath string
+
+	// ConfigDirectory holds path to config directory
 	ConfigDirectory string
 )
 
-const APP_CONFIG_DIRECTORY = "/.rundeck-zabbix/"
-const APP_CONFIG_NAME = "config"
-const APP_CONFIG_EXTENSION = "json"
+// AppConfigName is the config file constant for config
+const AppConfigName = "config"
 
 func init() {
-	UserHome = os.ExpandEnv("$HOME")
-	ConfigDirectory = UserHome + APP_CONFIG_DIRECTORY
-	ConfigPath = ConfigDirectory + APP_CONFIG_NAME + "." + APP_CONFIG_EXTENSION
+	ConfigDirectory = path.Join(os.ExpandEnv("$HOME"), ".rundeck-zabbix/")
+	ConfigPath = path.Join(ConfigDirectory, fmt.Sprintf("%s.json", AppConfigName))
+
 	if FileExists(ConfigDirectory) == false {
 		os.MkdirAll(ConfigDirectory, os.ModePerm)
 	}
 }
 
-func (object *Config) Save() {
+// Save object config
+func (object *Config) Save() error {
 	data, err := json.MarshalIndent(*object, "", "  ")
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("cannot marshal config. Error: %v", err)
 	}
+
 	err = ioutil.WriteFile(ConfigPath, data, 0644)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("cannot write to file. Error: %v", err)
 	}
+	return nil
 }
 
-func NewConfigFromFile() *Config {
+// NewConfigFromFile creates new config from file
+func NewConfigFromFile(configPath string) (*Config, error) {
 	config := Config{}
-	b, err := ioutil.ReadFile(ConfigPath)
+	b, err := ioutil.ReadFile(configPath)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("cannot read file. Error: %v", err)
 	}
 	err = json.Unmarshal(b, &config)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("cannot unmarshal json. Error: %v", err)
 	}
-	return &config
+	return &config, nil
+}
+
+// FileExists checks if file exists and returns a bool value
+func FileExists(name string) bool {
+	if _, err := os.Stat(name); !os.IsNotExist(err) {
+		return true
+	}
+	return false
 }
