26c92cb8c963687a854b2048847ed6ea70bf8a5b
Kosy Anyanwu <kosyfrances@gmail.com>
2019-03-18 23:27:57 +0100

    zabbix test: Test get triggers info and client creation

--- lib/zabbix/zabbix_test.go
+++ lib/zabbix/zabbix_test.go
@@ -35,7 +35,7 @@ func TestGetKey(t *testing.T) {
 
 // Tests that we can get Resource info from a Zabbix API call
 func TestGetHostsInfo(t *testing.T) {
-	expected := Results{
+	expected := HostResults{
 		{
 			HostID:      "10261",
 			Host:        "dummy-host",
@@ -47,7 +47,7 @@ func TestGetHostsInfo(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		res := struct {
-			Results `json:"result"`
+			HostResults `json:"result"`
 		}{expected}
 		err := json.NewEncoder(w).Encode(res)
 		if err != nil {
@@ -68,5 +68,108 @@ func TestGetHostsInfo(t *testing.T) {
 
 	if !(reflect.DeepEqual(res, expected)) {
 		t.Errorf("expected result to be %v but got %v", expected, res)
+	}
+}
+
+func TestGetTriggersInfo(t *testing.T) {
+	expected := TriggerResults{
+		{
+			Description: "Random trigger description",
+			Hosts: []TriggerHost{
+				TriggerHost{
+					Name: "dummy-host",
+				},
+			},
+		},
+	}
+
+	// mock api call
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		res := struct {
+			TriggerResults `json:"result"`
+		}{expected}
+		err := json.NewEncoder(w).Encode(res)
+		if err != nil {
+			t.Fatalf("unable to write response. error: %v", err)
+		}
+	}))
+	defer ts.Close()
+
+	a, err := CreateClientUsingAPIKey(ts.URL, "fake_zabbix_key")
+	if err != nil {
+		t.Fatalf("process ran with err %v,", err)
+	}
+
+	res, err := a.GetTriggersInfo()
+	if err != nil {
+		t.Fatalf("process ran with err %v, want result to be %v", err, expected)
+	}
+
+	if !(reflect.DeepEqual(res, expected)) {
+		t.Errorf("expected result to be %v but got %v", expected, res)
+	}
+}
+
+func TestCreateClientUsingAPIKey(t *testing.T) {
+	expected := &API{
+		URL: "fake_url",
+		Key: "fake_key",
+	}
+
+	// Ensure that client is not created without value for key
+	_, err := CreateClientUsingAPIKey(expected.URL, "")
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	// Ensure that client is not created without value for URL
+	_, err = CreateClientUsingAPIKey("", expected.Key)
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	// Ensure that client is not created without value for URL and key
+	_, err = CreateClientUsingAPIKey("", "")
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	// Ensure that client is can be created with value and key
+	a, err := CreateClientUsingAPIKey(expected.URL, expected.Key)
+	if err != nil {
+		t.Errorf("expected client to be %v but got %v", expected, a)
+	}
+}
+
+func TestCreateClientUsingAuth(t *testing.T) {
+	expected := &API{
+		URL:      "fake_url",
+		User:     "fake_user",
+		Password: "fake_password",
+	}
+
+	// Ensure that client is not created without value for URL
+	_, err := CreateClientUsingAuth("", expected.User, expected.Password)
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	// Ensure that client is not created without value for Username
+	_, err = CreateClientUsingAuth(expected.URL, "", expected.Password)
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	// Ensure that client is not created without value for Password
+	_, err = CreateClientUsingAuth(expected.URL, expected.User, "")
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	// Ensure that client is created with required values
+	a, err := CreateClientUsingAuth(expected.URL, expected.User, expected.Password)
+	if err != nil {
+		t.Errorf("expected client to be %v but got %v", expected, a)
 	}
 }
