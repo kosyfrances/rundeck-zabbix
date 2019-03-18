51ea000b5983cdda0d91de58bab93a5136da3d02
Kosy Anyanwu <kosyfrances@gmail.com>
2019-03-18 23:29:08 +0100

    utils: Add file dump function

--- lib/utils/utils.go
+++ lib/utils/utils.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 // MakeRequest makes an API request.
@@ -25,4 +26,20 @@ func MakeRequest(method, URL string, payload interface{}) (*http.Response, error
 	req.Header.Set("Content-type", "application/json")
 	// Send the request via a client
 	return http.DefaultClient.Do(req)
+}
+
+// DumpToFile dumps data, a list of bytes to file given.
+// If the filePath doesn't exist, it creates it, or appends to the file
+func DumpToFile(filePath string, data []byte) error {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("cannot open file. %v", err)
+	}
+
+	defer f.Close()
+
+	if _, err := f.Write(data); err != nil {
+		return fmt.Errorf("cannot write to file. %v", err)
+	}
+	return nil
 }
